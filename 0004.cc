2ede50ed4e83ecbae437b0d1823e226a549fff40
Forrest Yu <forrest.yu@gmail.com>
2011-11-22 00:27:46 +0800

    improved solution to problem 4
    improved the code after reading the first page of the official
    overview PDF
    
    before improve : 469965 compares
     after improve :   2304 compares

--- 0004.cc
+++ 0004.cc
@@ -9,32 +9,34 @@
 #include <iostream>
 #include <cstdio>
 
-bool ispalin(char* s, int n) {
-    for (int i = 0; i < n / 2; i++) {
-        if (s[i] != s[n - 1 - i])
-            return false;
-    }
-    return true;
-}
-
 bool ispalin(int x) {
-    char s[8];
-    int n = sprintf(s, "%d", x);
-    return ispalin(s, n);
+    int y = x;
+    int r = 0;
+    while (y) {
+        r = r * 10 + y % 10;
+        y /= 10;
+    }
+    return r == x;
 }
 
 int main(int argc, char* argv[]) {
     int _cnt_ = 0;
     int largest = 1;
     for (int k = 999; k > 30; k--) {
-        for (int i = 999, j = k; j < 1000; i--, j++) {
+        int flag = false;
+        for (int i = 999, j = k; j <= i; i--, j++) {
             ++_cnt_;
             int x = i * j;
-            if (x > largest && ispalin(x)) {
-                printf("%d: %d * %d\n", x, i, j);
-                largest = x;
+            if (x > largest) {
+                flag = true;
+                if (ispalin(x)) {
+                    printf("%d: %d * %d\n", x, i, j);
+                    largest = x;
+                }
             }
         }
+        if (!flag)  // no more product is larger than largest, so we know
+            break;  // we're done
     }
     printf("%d \033[37m(cost: %d compares)\033[0m\n", largest, _cnt_);
     return 0;
