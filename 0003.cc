c7fac4203370f5f6129d13c7b65cea91b99be774
Forrest Yu <forrest.yu@gmail.com>
2011-11-21 23:04:56 +0800

    improved solution to problem 3
    improved 0003.cc.
    it's still not as good as orange's solution, but the code is more
    readable now.

--- 0003.cc
+++ 0003.cc
@@ -10,56 +10,27 @@ bool is_prime(long long n) {
     for (long long k = 2; k * k <= n; k++)
         if (n % k == 0)
             return false;
-    std::cerr << "found prime " << n << "\n";
     return true;
 }
 
-void remove_factor(long long& n, long long k) {
-    std::cout << "factor " << k << "\n";
-    while (n % k == 0)
-        n /= k;
-}
-
-long long lpf_so_far(long long& n, long long k) {
-    long long m = n / k;
-    remove_factor(n, k);
-    if (is_prime(m)) {
-        remove_factor(n, m);
-        if (m > k)
-            return m;
-    }
+long long get_next_prime(long long x) {
+    long long k = x + 1;
+    while (!is_prime(k))
+        k++;
     return k;
-}
-
-long long find_next_factor(long long& n) {
-    static std::set<long long> primes;
-    for (std::set<long long>::iterator it = primes.begin();
-         it != primes.end(); ++it) {
-        if (n % *it == 0)
-            return lpf_so_far(n, *it);
-    }
-    long long k = primes.empty() ? 2 : (*primes.rbegin() + 1);
-    for (; k * k <= n; k++) {
-        if (is_prime(k)) {
-            primes.insert(k);
-            if (n % k == 0)
-                return lpf_so_far(n, k);
-        }
-    }
-    return n;
 }
 
 long long lpf(long long n) {
     long long largest = 1;
-    while (1) {
-        long long p = find_next_factor(n);
-        std::cerr << p << ',' << n << '\n';
-        if (p <= largest)
-            break;
-        if (p > largest)
-            largest = p;
+    for (long long prime = 2; prime <= n; prime = get_next_prime(prime)) {
+        if (n % prime == 0) {
+            while (n % prime == 0)
+                n /= prime;
+            if (prime > largest)
+                largest = prime;
+        }
     }
-    return std::max(largest, n);
+    return largest;
 }
 
 long long stupid_lpf(long long n) {
@@ -86,13 +57,13 @@ long long orange_lpf(long long n) {
 
 void test(long long n) {
     // std::cout << n << " : " << stupid_lpf(n) << "\n";
-    std::cout << n << " : " << lpf(n) << "\n";
+    //std::cout << n << " : " << lpf(n) << "\n";
     std::cout << "\033[31m" << n << " : " << orange_lpf(n) << "\033[0m\n";
 }
 
 int main(int argc, char* argv[]) {
-    test(13195);
+    //test(13195);
     test(600851475143);
-    test(337500);
+    //test(337500);
     return 0;
 }
