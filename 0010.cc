4b0c0ad9fbf7edf37c77a09aeacedf44a1a5b35f
Forrest Yu <forrest.yu@gmail.com>
2011-11-25 22:37:48 +0800

    improved solution to problem 10
    improved the code after reading The seive of Eratosthenes from
    the official overview PDF

--- 0010.cc
+++ 0010.cc
@@ -3,46 +3,59 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 // Find the sum of all the primes below two million.
 //
-// Answer: 
+// Answer: 142913828922
 
 #include <iostream>
 #include <vector>
+
 #include <cstdio>
+#include <cmath>
 #include <cassert>
 
-namespace {
-int _A[] = {2};
-std::vector<int> A(_A, _A + 1);
-bool is_prime(int x) {
-    int i = 0;
-    while (A[i] * A[i] <= x) {
-        if (x % A[i] == 0)
-            return false;
-        i++;
+class Prime {
+public:
+    Prime(long long N) : current_(2) {
+        // make sure there's at least one prime after A_[N - 1]
+        N += static_cast<long long>(sqrt(N + 1.0)) + 1;
+
+        for (long long i = 0; i < N; i++)
+            A_.push_back(i);
+        for (long long i = 2; i * i <= N; i++) {
+            if (A_[i] == 0)
+                continue;
+            for (long long j = i * 2; j < N; j += i)
+                A_[j] = 0;
+        }
     }
-    return true;
-}
-int get_next_prime(int x) {
-    assert(x == *A.rbegin());
-    int i = x + 1;
-    while (!is_prime(i))
-        i++;
-    A.push_back(i);
-    return i;
-}
-}
+    long long operator*() {
+        return A_[current_];
+    }
+    void inc() {
+        do {
+            ++current_;
+        } while (A_[current_] == 0);
+        assert(current_ < A_.size());
+    }
+    void operator++() {
+        inc();
+    }
+    void operator++(int) {
+        inc();
+    }
+private:
+    std::vector<long long> A_;
+    long long current_;
+};
 
 void test(int n) {
     long long sum = 0;
-    int prime = 2;
-    while (prime < n) {
-        sum += prime;
-        prime = get_next_prime(prime);
-    }
+    for (Prime p(n); *p < n; p++)
+        sum += *p;
     std::cout << n << " : " << sum << "\n";
 }
 
 int main(int argc, char* argv[]) {
+    //test(10);
     test(2000000);
     return 0;
 }
